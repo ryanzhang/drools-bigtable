102f263f2dc7d32025893be9ebc5b059a026e53a
Ryan Zhang <rzhang@redhat.com>
2021-02-17 15:48:47 +0800

    Add rule-template sample

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to run
+```bash
+mvn clean compile exec:java
+#Output
+Initial Kie Session elapsed time: 12469 
+false
+fired rules: 1 elapsed time: 524 
+Is Object Pass:false
+```
---
+++ myapp/src/main/java/com/myspace/App.java
@@ -0,0 +1,67 @@
+package com.myspace;
+
+import com.myspace.spreadsheet_decisiontable.ClientObject;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class App {
+
+    private KieContainer kieContainer;
+
+    public static void main(String[] args) {
+        App run = new App();
+        run.evaluateRules();
+    }
+
+    private  void evaluateRules() {
+        // get kie container
+        kieContainer = getContainer();
+
+        // start the stopwatch
+        // KieContainer kieContainer = KieServices.Factory.get().getKieClasspathContainer();
+        long start = System.currentTimeMillis();
+        KieSession ksession = kieContainer.newKieSession("mykiesession");
+        long end = System.currentTimeMillis();
+        System.out.format("Initial Kie Session elapsed time: %d \n",  (end - start));
+
+        start = System.currentTimeMillis();
+        ClientObject o1 = new ClientObject();
+        // ClientObject o2 = new ClientObject();
+        // ClientObject o3 = new ClientObject();
+        o1.setDescr("DangerObject9999");
+        // o2.setDescr("DangerObject99");
+        // o3.setDescr("DangerObject999");
+
+        ksession.insert(o1);
+        // ksession.insert(o2);
+        // ksession.insert(o3);
+
+        int fired = ksession.fireAllRules();
+        // stop watch
+        end = System.currentTimeMillis();
+
+        // print the result
+        System.out.format("fired rules: %d elapsed time: %d \n", fired, (end - start));
+        System.out.println("Is Object Pass:" + o1.isPass());
+
+        // if (ksession != null) {
+        //     ksession.getFactHandles().forEach(f -> System.out.println(ksession.getObject(f)));
+        //     ksession.dispose();
+        // }
+    }
+
+    private static KieContainer getContainer() {
+        KieServices kieServices = KieServices.Factory.get();
+
+        // Retrieve the decision project (kjar) from classpath
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        // System.out.println(kieContainer.verify().getMessages().toString());
+
+        // Dynamically pull the decision project from Maven repo
+        // ReleaseId releaseId = kieServices.newReleaseId("com.myspace","spreadsheet-decisiontable", "1.0.0-SNAPSHOT");
+        // KieContainer kieContainer = kieServices.newKieContainer(releaseId);
+        return kieContainer;
+    }
+}
